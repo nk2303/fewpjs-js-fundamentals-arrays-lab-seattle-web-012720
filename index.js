373d958c885bd470fd6057b4f4d077c3b21533d8
github username <github email address>
2020-03-09 15:02:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"]
+const prepend = ["Milo", "Otis", "Garfield"]
+const removeLast = ["Milo", "Otis", "Garfield"]
+const removeFirst = ["Milo", "Otis", "Garfield"]
+
+// function append(p1, p2) {
+//     return p1 * p2;
+//   }
+
+// function prepend(p1, p2) {
+//     return p1 * p2;
+//   }
+
+// function removeLast(p1, p2) {
+//     return p1 * p2;
+//   }
+
+// function removeFirst(p1, p2) {
+//     return p1 * p2;
+//   }
+
+append.push("Odie")
+prepend.unshift("Odie")
+removeLast.pop()
+removeFirst.shift()
